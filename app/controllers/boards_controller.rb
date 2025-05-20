538109cddccf41071db8f3fe87d61fe50e53a85b
brianyeh1 <nyeh@chicagobooth.edu>
2025-05-20 06:07:14 +0000

    new listing page skeleton

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -6,4 +6,33 @@ class BoardsController < ApplicationController
 
     render({ :template => "items/index" })
   end
+
+  def add
+
+    render({ :template => "items/add" })
+  end
+
+  def create
+    the_item = Item.new
+    the_item.category = params.fetch("category")
+    the_item.name = params.fetch("name")
+    the_item.description = params.fetch("description")
+    the_item.picture_1 = params.fetch("picture_1")
+    the_item.picture_2 =  params.fetch("picture_2")
+    the_item.picture_3 = params.fetch("picture_3")
+    the_item.picture_4 = params.fetch("picture_4")
+    the_item.picture_5 = params.fetch("picture_5")
+    the_item.price = params.fetch("price")
+    the_item.payment_method = params.fetch("payment_method")
+    the_item.delivery_method = params.fetch("delivery_method")
+    the_item.seller_id = current_user.id
+
+   # if the_item.valid?
+   #   the_item.save
+   #   redirect_to("/item/#{the_item.id}"), { :notice => "Listing created successfully." })
+   # else
+   #   redirect_to("/new_listing", { :alert => the_item.errors.full_messages.to_sentence })
+   # end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
-  get("/", { :controller => "boards", :action => "index"})
+#Listing pages
+
+get("/", { :controller => "boards", :action => "index"})
   
+#Create new listing
+
+get("/new_listing", { :controller => "boards", :action => "add"})
+post("/create_listing", { :controller => "boards", :action => "create" })
+
 end

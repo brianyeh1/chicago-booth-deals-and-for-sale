b8157666fa26b6625d5824c2f337848852c7f69a
brianyeh1 <nyeh@chicagobooth.edu>
2025-05-29 19:51:04 +0000

    added paths to redirect non-signed in users to sign in for applicable pages

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -71,37 +71,41 @@ class BoardsController < ApplicationController
   end
 
   def create
-    the_item = Item.new
-    the_item.category = params.fetch("category")
-    the_item.name = params.fetch("name")
-    the_item.description = params.fetch("description")
-    if params.fetch("picture_1", nil)
-      the_item.picture_1 = params.fetch("picture_1")
-    end
-    if params.fetch("picture_2", nil)
-    the_item.picture_2 =  params.fetch("picture_2")
-    end
-    if params.fetch("picture_3", nil)
-    the_item.picture_3 = params.fetch("picture_3")
-    end
-    if params.fetch("picture_4", nil)
-    the_item.picture_4 = params.fetch("picture_4")
-    end
-    if params.fetch("picture_5", nil)
-    the_item.picture_5 = params.fetch("picture_5")
-    end
-    raw_price = params.fetch("price")
-    cleaned_price = raw_price.delete("$")
-    the_item.price = cleaned_price.to_f
-    the_item.payment_method = params.fetch("payment_method")
-    the_item.delivery_method = params.fetch("delivery_method")
-    the_item.seller_id = current_user.id
-
-    if the_item.valid?
-      the_item.save
-      redirect_to("/item/#{the_item.id}", { :notice => "Listing created successfully." })
+    if current_user == nil
+      redirect_to("/users/sign_in", { :alert => "Please sign in to create a new listing"} )
     else
-      redirect_to("/new_listing", { :alert => the_item.errors.full_messages.to_sentence })
+      the_item = Item.new
+      the_item.category = params.fetch("category")
+      the_item.name = params.fetch("name")
+      the_item.description = params.fetch("description")
+      if params.fetch("picture_1", nil)
+        the_item.picture_1 = params.fetch("picture_1")
+      end
+      if params.fetch("picture_2", nil)
+        the_item.picture_2 =  params.fetch("picture_2")
+      end
+      if params.fetch("picture_3", nil)
+        the_item.picture_3 = params.fetch("picture_3")
+      end
+      if params.fetch("picture_4", nil)
+        the_item.picture_4 = params.fetch("picture_4")
+      end
+      if params.fetch("picture_5", nil)
+        the_item.picture_5 = params.fetch("picture_5")
+      end
+      raw_price = params.fetch("price")
+      cleaned_price = raw_price.delete("$")
+      the_item.price = cleaned_price.to_f
+      the_item.payment_method = params.fetch("payment_method")
+      the_item.delivery_method = params.fetch("delivery_method")
+      the_item.seller_id = current_user.id
+
+      if the_item.valid?
+        the_item.save
+        redirect_to("/item/#{the_item.id}", { :notice => "Listing created successfully." })
+      else
+        redirect_to("/new_listing", { :alert => the_item.errors.full_messages.to_sentence })
+      end
     end
   end
 
--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,11 +1,15 @@
 class MessageController < ApplicationController
  
   def create
+    if current_user == nil
+      redirect_to("/users/sign_in", { :alert => "Please sign in message a seller"} )
+    else
     @item_id = params.fetch("path_id")
     @matching_item = Item.where({ :id => @item_id }).at(0)
     matching_seller = User.where({ :id => @matching_item.seller_id })
     @the_seller = matching_seller.at(0)
     render({ :template => "message/create" })
+    end
   end
   
   def sent
@@ -43,25 +47,33 @@ class MessageController < ApplicationController
   end
 
   def inbox
-    @user_id = current_user.id
-    seller_updates = Message.where({ :seller_id => @user_id }).group(:item_id).maximum(:updated_at)
-    @seller_unique_messages = seller_updates.map   do |item, ts|
-        Message.where({ :seller_id => @user_id, :item_id => item, :updated_at => ts }).at(0)
+    if current_user == nil
+      redirect_to("/users/sign_in", { :alert => "Please sign in to view your inbox"} )
+    else
+      @user_id = current_user.id
+      seller_updates = Message.where({ :seller_id => @user_id }).group(:item_id).maximum(:updated_at)
+      @seller_unique_messages = seller_updates.map   do |item, ts|
+          Message.where({ :seller_id => @user_id, :item_id => item, :updated_at => ts }).at(0)
       end
 
-    buyer_updates = Message.where({ :buyer_id => @user_id }).group(:item_id).maximum(:updated_at)
-    @buyer_unique_messages = buyer_updates.map   do |item, ts|
-      Message.where({ :buyer_id => @user_id, :item_id => item, :updated_at => ts }).at(0)
-    end
+      buyer_updates = Message.where({ :buyer_id => @user_id }).group(:item_id).maximum(:updated_at)
+      @buyer_unique_messages = buyer_updates.map   do |item, ts|
+        Message.where({ :buyer_id => @user_id, :item_id => item, :updated_at => ts }).at(0)
+      end
 
-    render({ :template => "message/inbox" })
+      render({ :template => "message/inbox" })
+    end
   end
 
   def view
-  item_id = params.fetch("path_id")
-  @the_item = Item.where({ :id => item_id }).at(0)
-  @the_messages = Message.where({ :item_id => item_id }).order({ :updated_at => :asc })
-  render({ :template => "message/view" })
+    if current_user == nil
+        redirect_to("/users/sign_in", { :alert => "Please sign in to view your message"} )
+    else
+      item_id = params.fetch("path_id")
+      @the_item = Item.where({ :id => item_id }).at(0)
+      @the_messages = Message.where({ :item_id => item_id }).order({ :updated_at => :asc })
+      render({ :template => "message/view" })
+    end
   end
 
 end
